ad1b6ec45562ab1144b53fdaa30caf9b83ccd3bb
gayoung lee <ken604@naver.com>
2025-01-30 00:52:41 +0900

    공통 컴포넌트 구현 완료 예시 완료

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Global } from "@emotion/react";
 import globalStyles from "./styles/globalStyles";
 
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
